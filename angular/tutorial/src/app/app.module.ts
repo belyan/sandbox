3d190b421edca41f014377e069dd376f0cc4e6a6
Yuri Beliakov <yuri.beliakov@gmail.com>
2017-07-05 02:40:33 +0300

    Angular Tutorial. Routing

--- angular/tutorial/src/app/app.module.ts
+++ angular/tutorial/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'heroes', component: HeroesComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'detail/:id', component: HeroDetailComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
--- angular/tutorial/src/app/hero-detail.component.ts
+++ angular/tutorial/src/app/hero-detail.component.ts
@@ -1,20 +1,32 @@
-import { Component, Input } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html'
 })
 
-export class HeroDetailComponent {
-  @Input() hero: Hero
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
